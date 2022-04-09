826dd3e8f800acb4127116d7a94bbcebb7c41bcf
ikuma-t <tadokorodev@gmail.com>
2022-04-09 15:11:22 +0900

    feat: bundle gem

---
+++ lib/gem_rbs_collection_sample.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem_rbs_collection_sample/version"
+
+module GemRbsCollectionSample
+  class Error < StandardError; end
+  # Your code goes here...
+end
