e8f61217e8f5a4f2ba0fd866b003470e1edeeda9
ikuma-t <tadokorodev@gmail.com>
2022-04-09 15:12:30 +0900

    feat: add sample method and signature

--- lib/gem_rbs_collection_sample.rb
+++ lib/gem_rbs_collection_sample.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "gem_rbs_collection_sample/version"
+require 'date'
 
 module GemRbsCollectionSample
-  class Error < StandardError; end
   # Your code goes here...
+  def greet_with_date(message)
+    "Hello, #{message}, Today is #{Date.new}"
+  end
 end
